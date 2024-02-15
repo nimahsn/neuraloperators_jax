b231844d1cde77b30af630852d44ed0962225814
Nima Hosseini Dashtbayaz <nimahsn@hotmail.com>
2024-02-15 16:42:19 -0500

    2d FNO classes - WIP

--- models/fno.py
+++ models/fno.py
@@ -4,7 +4,7 @@ import jax.numpy as jnp
 from jax.nn.initializers import glorot_normal
 import equinox as eqx
 from jaxtyping import ArrayLike, Complex, Array, PRNGKeyArray
-from typing import Callable
+from typing import Callable, List, Tuple
 
 from functools import partial
 
@@ -119,4 +119,94 @@ class FNO1d(eqx.Module):
         for block in self.fourier_blocks:
             x = block(x)
         return self.projection_output(x)
+    
+class SpectraclConv2d(eqx.Module):
+    weight_r: Array
+    in_c: int = eqx.field(static=True)
+    out_c: int = eqx.field(static=True)
+    k_modes: Tuple[int] = eqx.field(static=True)
+
+    def __init__(self, in_c: int, out_c: int, k_modes: List[int], key: PRNGKeyArray, initializer=jax.nn.initializers.glorot_normal):
+        self.in_c = in_c
+        self.out_c = out_c
+        k_modes[-1] = k_modes[-1]//2 + 1
+        self.k_modes = tuple(k_modes)
+        self.weight_r = initializer()(key, (in_c, out_c, *self.k_modes), jnp.complex64)
+
+    def __call__(self, x: ArrayLike):
+        x_ft = jnp.fft.rfft2(x) # shape: (in_c, d1, d2//2 + 1)
+        x_ft = x_ft[..., :self.k_modes[-2], :self.k_modes[-1]]
+        out_ft = jnp.einsum('ikl,iokl->okl', x_ft, self.weight_r)
+        return jnp.fft.irfft2(out_ft, s=(self.out_c, *x.shape[1:]))
+    
+class FNOBlock2d(eqx.Module):
+    """
+    A single block of the FNO model. The block consists of a spectral convolutional layer and a residual local transform.
+    """
+
+    spec_conv: SpectraclConv2d
+    residual_local_transform: eqx.nn.Conv2d
+    activation: Callable = eqx.field(static=True)
+    in_channels: int = eqx.field(static=True)
+    out_channels: int = eqx.field(static=True)
+
+    def __init__(self, k_modes: List[int], in_channels: int, out_channels: int, activation: Callable, key: PRNGKeyArray):
+        self.activation = activation
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        key, subkey = random.split(key)
+        self.spec_conv = SpectraclConv2d(in_channels, out_channels, k_modes, key)
+        self.residual_local_transform = eqx.nn.Conv2d(in_channels, out_channels, 1, 1, key=subkey)
+
+    def __call__(self, x: ArrayLike) -> ArrayLike:
+        """
+        Apply the layer to the input.
+
+        Args:
+            x: ArrayLike
+                The input tensor. Should be of shape `(in_c, d1, d2)`.
+
+        Returns:
+            ArrayLike
+                The output tensor. Will be of shape `(out_c, d1, d2)`.
+        """
+        return self.activation(self.spec_conv(x) + self.residual_local_transform(x))
+
+
+
+class FNO2d(eqx.Module):
+    fourier_blocks: List[FNOBlock2d]
+    projection_input: eqx.nn.Conv2d
+    projection_output: eqx.nn.Conv2d
+    in_channels: int = eqx.field(static=True)
+    width: int = eqx.field(static=True)
+    out_channels: int = eqx.field(static=True)
+
+    def __init__(self, in_channels: int, out_channels: int, width: int, k_modes: List[int], depth: int, activation: Callable, key: PRNGKeyArray, p_in: eqx.nn.Conv2d=None, p_out: eqx.nn.Conv2d=None):
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.width = width
+        key, *subkeys = random.split(key, depth + 3)
+        self.projection_input = p_in if p_in is not None else eqx.nn.Conv2d(in_channels, width, 1, 1, key=subkeys[0])
+        self.projection_output = p_out if p_out is not None else eqx.nn.Sequential([eqx.nn.Conv2d(width, 128, 1, 1, key=subkeys[1]),
+                                                                                    eqx.nn.Conv2d(128, out_channels, 1, 1, key=subkeys[2])])
+        self.fourier_blocks = [FNOBlock2d(k_modes, width, width, activation, subkey) for subkey in subkeys[2:]]
+
+    def __call__(self, x: ArrayLike) -> ArrayLike:
+        """
+        Apply the layer to the input.
+
+        Args:
+            x: ArrayLike
+                The input tensor. Should be of shape `(in_c, d1, d2)`.
+
+        Returns:
+            ArrayLike
+                The output tensor. Will be of shape `(out_c, d1, d2)`.
+        """
+        x = self.projection_input(x)
+        for block in self.fourier_blocks:
+            x = block(x)
+        return self.projection_output(x)
+    
     
